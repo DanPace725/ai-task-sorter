52f5c97c3b3957783aec06accc66ec5b9aa356c9
DanPace725 <118482982+DanPace725@users.noreply.github.com>
2023-01-24 09:33:50 -0800

    main and boiler commit

---
+++ boiler.py
@@ -0,0 +1,32 @@
+import streamlit as st
+import transformers
+
+# Connect to database
+from sqlalchemy import create_engine
+engine = create_engine('sqlite:///tasks.db')
+
+# Load pre-trained model
+model = transformers.AutoModelForSequenceClassification.from_pretrained("bert-base-cased")
+
+def classify_task(task):
+    # classify the task based on importance
+    pass
+
+def main():
+    st.title("Task Organizer App")
+
+    task = st.text_input("Enter task and context")
+    if st.button("Submit"):
+        importance = classify_task(task)
+        # Store task in the database
+        pass
+
+    # Retrieve tasks from the database and display them
+    st.write("Tasks:")
+    tasks = []
+    for task in tasks:
+        st.write("- {} (Importance: {})".format(task.name, task.importance))
+        st.checkbox("Completed")
+
+if __name__=="__main__":
+    main()
